a7e21539250955d76dfbc039dc25f38f586d2889
Srijan Saurav <srijan@deepsource.io>
2020-03-23 15:57:20 +0530

    Create assert_outside_test.py

---
+++ assert_outside_test.py
@@ -0,0 +1,15 @@
+# inside a func def
+def some_function(args):
+    """Docstring."""
+    assert 1 > 2, (
+        "Some message"
+    )
+
+    if args != []:
+        raise AssertionError
+
+some_function(123)
+
+
+#Comment
+assert {} is not []
