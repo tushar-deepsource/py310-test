f185740cfdf39335bfecdd20353767dbcced2127
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2021-03-21 18:19:27 +0530

    Create tst.sh

---
+++ shell-test/tst.sh
@@ -0,0 +1,8 @@
+echo "$"
+echo 'This is not how it\'s done'
+echo 'Nothing so needs reforming as other people's habits.'
+[ -e ~/.bashrc -a ( -x /bin/dash -o -x /bin/ash ) ]
+for $var in *
+do
+  echo "$var"
+done
