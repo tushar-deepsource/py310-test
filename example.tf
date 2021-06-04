839826966eca5d6ebb07936bce1b4ce8493e10bc
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2021-06-04 11:39:20 +0530

    Update example.tf

--- example.tf
+++ example.tf
@@ -18,3 +18,14 @@ resource "azurerm_managed_disk" "source" {
         enabled = false
     }
 }
+
+
+resource "aws_instance" "web" {
+  ami                  = "ami-b73b63a0"
+  instance_type        = "t1.micro" # previous instance type!
+  iam_instance_profile = "app-service"
+
+  tags {
+    Name = "HelloWorld"
+  }
+}
