4843d581ca997335c6b141c204cbceb74d5375cd
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2021-06-04 11:44:36 +0530

    Update conf.tf

--- conf.tf
+++ conf.tf
@@ -1,4 +1,4 @@
-esource "aws_instance" "web" {
+resource "aws_instance" "web" {
   ami                  = "ami-b73b63a0"
   instance_type        = "t1.micro"
   iam_instance_profile = "app-service"
@@ -6,3 +6,4 @@ esource "aws_instance" "web" {
   tags {
     Name = "HelloWorld"
   }
+}
