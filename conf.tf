e1ecc11ba35608da7039c2f894aad82bd5b16f75
Srijan Saurav <68371686+srijan-deepsource@users.noreply.github.com>
2021-06-04 11:47:39 +0530

    Update conf.tf

--- conf.tf
+++ conf.tf
@@ -1,9 +1,21 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "webber" {
   ami                  = "ami-b73b63a0"
   instance_type        = "t1.micro"
   iam_instance_profile = "app-service"
 
   tags {
-    Name = "HelloWorld"
+    Name = "HeyWorld"
   }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.6.17"
+  instance_class       = "db.t1.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "bar"
+  db_subnet_group_name = "my_database_subnet_group"
+  parameter_group_name = "default.mysql5.6"
 }
